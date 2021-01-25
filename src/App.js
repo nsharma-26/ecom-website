f9693833de0807a3d115da7255f0c02189a70c52
Neha Sharma <Neha Sharma@LAPTOP-C0L7II0J>
2021-01-25 14:39:34 +0530

    commit header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './container/homepage/homepage.component';
 import ShopPage from './container/shop/shop';
+import Header from './component/header/header'
 // import OrderSearch from './container/OrderSearch/OrderSearch';
 import { Switch, Route } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+          <Header />
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
