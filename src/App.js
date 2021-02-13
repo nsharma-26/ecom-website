cdb9e3a358c7e5b1e1b39945012826c56ca70cbf
Neha Sharma <Neha Sharma@LAPTOP-C0L7II0J>
2021-02-13 12:42:35 +0530

    added checkout page functionality

--- src/App.js
+++ src/App.js
@@ -1,76 +1,82 @@
 import React from 'react';
-import { connect } from 'react-redux'
-
-import HomePage from './container/homepage/homepage.component';
-import ShopPage from './container/shop/shop';
-import Header from './component/header/header'
-// import OrderSearch from './container/OrderSearch/OrderSearch';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from './container/Login/Login'
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import { setCurrentUser } from './redux/user/user.actions'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { ThemeProvider } from 'react-bootstrap';
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import CheckoutPage from './pages/checkout/checkout';
+
+import Header from './components/header/header';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-  // constructor(){
-  //   super();
+  unsubscribeFromAuth = null;
 
-  //   this.state = {
-  //     currentUser:null
-  //   }
-  // }
-
-  unsubscribeFromAuth = null
-  componentDidMount(){
+  componentDidMount() {
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      //this.setState({currentUser:user})
-      //createUserProfileDocument(user)
-      //console.log(user);
-      if(userAuth){
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
+
         userRef.onSnapshot(snapShot => {
-          this.props.setCurrentUser({
-            
-              id:snapShot.id,
-              ...snapShot.data()
-            
-          })
-        })
-        //console.log(this.state);
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
       }
+
       setCurrentUser(userAuth);
-    })
+    });
   }
 
-  componentWillUnmount(){
-    this.unsubscribeFromAuth()
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div>
-          <Header/>
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/'/>) : (<Login/>)} />
-          </Switch>
-        
-        {/* <OrderSearch /> */}
-        </div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
     );
   }
 }
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
-})
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser : user => dispatch(setCurrentUser(user))
-})
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/component/cart-icon/cart-icon.js
+++ src/component/cart-icon/cart-icon.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { toggleCartHidden } from '../../redux/cart/cart.actions'
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import React from 'react';
+import { connect } from 'react-redux';
 
-import './cart-icon.scss'
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({toggleCartHidden}) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon'/>
-        <span className="item-count">0</span>
-    </div>
-)
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.scss';
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+  <div className='cart-icon' onClick={toggleCartHidden}>
+    <ShoppingIcon className='shopping-icon' />
+    <span className='item-count'>{itemCount}</span>
+  </div>
+);
+
 const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartIcon);
